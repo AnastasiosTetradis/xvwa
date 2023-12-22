1545b37ff8f54229bc3ee7ed671a1155bb1d874f
Anastasios Tetradis <anastasios.tetradis@gmail.com>
2023-12-22 22:41:18 +0200

    Add password protection to config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ $host = "localhost";
 $dbname = 'xvwa';
 $user = "root";
 $pass = "";
+$pass = getenv('VerySecurePasswordIAssureYou');
 $conn = new mysqli($host,$user,$pass,$dbname);
 $conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
